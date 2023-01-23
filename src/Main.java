2c62773f13f43a99518ed2aa194e32c1c88610a4
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-23 10:40:15 +0300

    massiv-zadacha1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        int i;
+        int b;
+        int c;
+        System.out.println("задача1");
+        int []chislo=new int[3];
+        chislo[0]=1;
+        chislo[1]=2;
+        chislo[2]=3;
+        System.out.println(chislo[0]);
+        System.out.println(chislo[1]);
+        System.out.println(chislo[2]);
+
+
+    }
+}
