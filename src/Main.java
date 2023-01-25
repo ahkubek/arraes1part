f084bc74c21c7f219987fbc7561c8caadf6babca
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-25 22:44:21 +0300

    massiv-zadacha3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,54 @@
 public class Main {
     public static void main(String[] args) {
         //task1
-        int i;
-        int b;
-        int c;
+
         System.out.println("задача1");
-        int []chislo=new int[3];
-        chislo[0]=1;
+        int[] chislo = new int[3];
+        for (int i = 0; i < chislo.length; i++) {
+            chislo[i] = i + 1;
+            System.out.println(chislo[i]);
+
+        }
+/*        chislo[0]=1;
         chislo[1]=2;
-        chislo[2]=3;
-        System.out.println(chislo[0]);
+        chislo[2]=3;*/
+        System.out.println();
+/*        System.out.println(chislo[0]);
         System.out.println(chislo[1]);
-        System.out.println(chislo[2]);
+        System.out.println(chislo[2]);*/
+        double[] nomer = {1.57, 7.654, 9.986};
+        for (int i = 0; i < nomer.length; i++) {
+            System.out.println(nomer[i]);
+
+        }
+        boolean[] proizvolno = {true, false};
+        for (int i = 0; i < proizvolno.length; i++) {
+            System.out.println(proizvolno[i]);
+
+        }
+        //task2
+        System.out.println();
+        System.out.println("Задача2");
+        for (int i = 0; i < chislo.length; i++) {
+            chislo[i] = i + 1;
+            System.out.print(chislo[i]);
+            if (i != chislo.length - 1) {
+                System.out.print(",");
+            }
 
 
-    }
-}
+        }
+        //task3
+        System.out.println();
+        System.out.println("Задача3");
+        for (int i = chislo.length -1;i>=0;i--) {
+            System.out.print(chislo[i]);
+            if (i!=0){
+                System.out.print(",");
+            }
+
+        }
+
+            }
+
+        }
